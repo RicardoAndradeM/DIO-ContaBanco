fb217dfea299d3c793328aace981599d7087fdcb
Ricardo Andrade <ricardo.maia12@gmail.com>
2025-08-01 13:49:49 -0300

    Feat: Add business logic.
     Add the code necessary for the software to meet the exercise specifications.
    
     Changes to be committed:
    	modified:   build.gradle.kts
    	modified:   src/main/java/io/github/ricardoandradem/ContaTerminal.java

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,6 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<JavaExec> {
+    standardInput = System.`in`
 }
--- src/main/java/io/github/ricardoandradem/ContaTerminal.java
+++ src/main/java/io/github/ricardoandradem/ContaTerminal.java
@@ -1,7 +1,29 @@
 package io.github.ricardoandradem;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
+
+    private static final String PROMPT_GET_NUMERO = "Por favor, digite o número da conta: ";
+    private static final String PROMPT_GET_AGENCIA = "Por favor, digite o número da Agência: ";
+    private static final String PROMPT_GET_NOME = "Por favor, digite seu nome: ";
+    private static final String PROMPT_GET_SALDO = "Por favor, digite o saldo: ";
+    private static final String OUTPUT_MESSAGE =
+            "\nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %s,\n" +
+                    "conta %d e seu saldo %.2f já está disponível para saque.\n";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print(PROMPT_GET_NUMERO);
+        int numero = input.nextInt();
+        System.out.print(PROMPT_GET_AGENCIA);
+        String agencia = input.next();
+        System.out.print(PROMPT_GET_NOME);
+        String nome = input.next();
+        System.out.print(PROMPT_GET_SALDO);
+        int saldo = (int) (input.nextFloat() * 100);
+
+        System.out.printf(OUTPUT_MESSAGE, nome, agencia, numero, ((float) saldo / 100 ));
     }
 }
